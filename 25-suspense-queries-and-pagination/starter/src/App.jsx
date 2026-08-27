43b5751c4a645ea1f417511aceaf656dfdf53706
Timothy Karani <kartimothy@gmail.com>
2026-08-27 06:19:52 +0300

    feat: done with exercise #25

--- 25-suspense-queries-and-pagination/starter/src/App.jsx
+++ 25-suspense-queries-and-pagination/starter/src/App.jsx
@@ -1,42 +1,70 @@
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { QueryErrorResetBoundary, useSuspenseQuery } from "@tanstack/react-query";
+import React, { useState, useTransition } from "react";
 import { fetchRepoList } from "./api";
+import { ErrorBoundary } from "react-error-boundary";
 
 function useRepos(sort) {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: ["repos", sort],
     queryFn: () => fetchRepoList(sort),
     staleTime: 30_000,
+    retry: 1,
   });
+}
+
+function ErrorFallback({ resetErrorBoundary }) {
+  return <div className="error">
+    <p>
+      Uh Oh! Something went wrong
+    </p>
+
+    <button onClick={resetErrorBoundary}>Retry</button>
+  </div>
 }
 
 export default function App() {
   const [sort, setSort] = useState("stars");
-  const reposQuery = useRepos(sort);
+  const [isPreviousData, startTransition] = useTransition();
 
   return (
     <main className="page">
       <h1>Suspense Search and Pagination</h1>
       <label>
         Sort
-        <select value={sort} onChange={(e) => setSort(e.target.value)}>
+        <select value={sort} onChange={(e) => {
+          startTransition(() => setSort(e.target.value))
+        }}>
           <option value="stars">Stars</option>
           <option value="updated">Updated</option>
           <option value="full_name">Name</option>
         </select>
       </label>
 
-      {/* TODO: refactor this screen to Suspense + ErrorBoundary */}
-      {reposQuery.isPending ? <p>Loading repos...</p> : null}
-      {reposQuery.isError ? <p>Something went wrong.</p> : null}
-      <ul className="list">
-        {reposQuery.data?.map((repo) => (
-          <li key={repo.id}>
-            <strong>{repo.name}</strong>
-            <span>{repo.stargazers_count} stars</span>
-          </li>
-        ))}
-      </ul>
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary onReset={reset} FallbackComponent={ErrorFallback}>
+            <React.Suspense fallback={<p>Loading the repos...</p>}>
+              <Repos sort={sort} isPreviousData={isPreviousData} />
+            </React.Suspense>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
     </main>
   );
+}
+
+function Repos({ sort, isPreviousData }) {
+  const reposQuery = useRepos(sort);
+
+  return (
+    <ul className="list" style={{ opacity: isPreviousData ? 0.5 : 1 }}>
+      {reposQuery.data.map((repo) => (
+        <li key={repo.id}>
+          <strong>{repo.name}</strong>
+          <span>{repo.stargazers_count} stars</span>
+        </li>
+      ))}
+    </ul>
+  )
+
 }
